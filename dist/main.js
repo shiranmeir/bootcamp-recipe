67c851e8d0f95528a87e09d09397b6fcc559950a
shiranmeir <shiranmeir7@gmail.com>
2019-04-04 13:21:20 +0300

    step 4

--- dist/main.js
+++ dist/main.js
@@ -1,13 +1,13 @@
 $("#button").on("click", function(){
     let newFood = $("#inputFood").val()
-    $.get(`/sanity/${newFood}`, function(foods){
-        render(foods)
+    $.get(`/recipes/${newFood}`, function(foods){
+        console.log(foods)
     })
 })
 
-const render= function(foods){
-    let source = $('#arrayid-template').html();
-    let template = Handlebars.compile(source);
-    let newHTML = template({players});
-    $('.container').append(newHTML);
-}
+// const render= function(foods){
+//     let source = $('#arrayid-template').html();
+//     let template = Handlebars.compile(source);
+//     let newHTML = template({players});
+//     $('.container').append(newHTML);
+// }
