4f6c30bf28e0e49dfb8ab40703f322942a052a35
shiranmeir <shiranmeir7@gmail.com>
2019-04-04 13:15:28 +0300

    step 3

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,13 @@
+$("#button").on("click", function(){
+    let newFood = $("#inputFood").val()
+    $.get(`/sanity/${newFood}`, function(foods){
+        render(foods)
+    })
+})
 
-
-// const render= function(players){
-//     let source = $('#arrayid-template').html();
-//     let template = Handlebars.compile(source);
-//     let newHTML = template({players});
-//     $('.container').append(newHTML);
-// }
+const render= function(foods){
+    let source = $('#arrayid-template').html();
+    let template = Handlebars.compile(source);
+    let newHTML = template({players});
+    $('.container').append(newHTML);
+}
--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express')
+const request = require('request')
 const path = require('path')
-const bodyParser = require('body-parser')
-const api = require('./server/routes/api')
-
 
 const app = express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
-app.use('/', api)
 
 
-const port = 1000
+app.get('/sanity', function (req, res) {
+    res.send("OK")
+})
+
+
+
+app.get('/recipes/:food', function (req, res) {
+    let food = req.params.food
+    request(`http://www.recipepuppy.com/api/?q=${food}`, function (err, response){
+    let f=JSON.parse(response.body)
+    let foodies=f.results
+    foodies.forEach(f=>f.ingredients = f.ingredients.split(', '))
+    res.send(foodies)
+    console.log(foodies)
+    })
+})
+
+const port = 8080
 app.listen(port, function () {
     console.log(`Server running on ${port}`)
 })
